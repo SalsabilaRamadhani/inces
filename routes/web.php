ea3f8399df6be9f6c1f8e87c40f6efbbfcdd9f88
SalsabilaRamadhani <97salsa.shy@gmail.com>
2025-01-11 01:34:50 +0700

    add upload media

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -25,8 +26,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('chirps', ChirpController::class)
-->only(['index', 'store', 'update', 'destroy'])
-->middleware(['auth', 'verified']);
 
-require __DIR__.'/auth.php';
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+
+Route::get('/upload', function () {
+    return Inertia::render('Upload');
+});
+
+Route::get('/chirper', function () {
+    return Inertia::render('Chirper');
+})->middleware(['auth', 'verified']); // Tambahkan middleware jika diperlukan
+
+Route::post('/upload', [UploadController::class, 'upload'])->middleware('web');
+
+
+require __DIR__ . '/auth.php';
